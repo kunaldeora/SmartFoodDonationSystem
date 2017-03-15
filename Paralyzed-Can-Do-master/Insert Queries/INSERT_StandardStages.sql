4a232cd02e3cd9bd7e458cd11dbf0c7ec139cf52
kunaldeora <deora.k@husky.neu.edu>
2017-03-15 18:08:16 -0400

    removing

--- Paralyzed-Can-Do-master/Insert Queries/INSERT_StandardStages.sql
+++
@@ -1,5 +0,0 @@
-insert into standardstages(stageName) values("Awake with Mental Activity");
-insert into standardstages(stageName) values("Awake and Resting");
-insert into standardstages(stageName) values("Sleeping");
-insert into standardstages(stageName) values("Deep Sleep");
-
